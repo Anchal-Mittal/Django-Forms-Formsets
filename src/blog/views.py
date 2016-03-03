339aa6fb0e261750b961e98668c49cd1d9a138f8
CFE <hello@teamcfe.com>
2016-03-02 20:43:32 -0800

    8 - Collecting Data via HTML Form

--- src/blog/views.py
+++ src/blog/views.py
@@ -6,4 +6,10 @@ from .forms import TestForm
 
 def home(request):
     form = TestForm()
+    if request.method == "POST":
+        print(request.POST)
+        print(request.POST.get("username")) #None
+        #print(request.POST["username2"]) #Raise error
+    elif request.method == "GET":
+        print(request.GET)
     return render(request, "forms.html", {"form": form})
