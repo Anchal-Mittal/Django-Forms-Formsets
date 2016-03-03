f665f70dd5c491893aaa6030b4cd04646200e52f
CFE <hello@teamcfe.com>
2016-03-02 20:50:43 -0800

    9 - Collecting Data via Django Form

--- src/blog/views.py
+++ src/blog/views.py
@@ -5,11 +5,19 @@ from django.shortcuts import render
 from .forms import TestForm
 
 def home(request):
-    form = TestForm()
-    if request.method == "POST":
-        print(request.POST)
-        print(request.POST.get("username")) #None
-        #print(request.POST["username2"]) #Raise error
-    elif request.method == "GET":
-        print(request.GET)
+    form = TestForm(request.POST or None)
+    if form.is_valid():
+        print(form.cleaned_data)
+        print(form.cleaned_data.get("some_text"))
+        print(form.cleaned_data.get("email"))
+        print(form.cleaned_data.get("email2"))
+
+    # if request.method == "POST":
+    #     form = TestForm(request.POST)
+    #     print(request.POST)
+    #     print(request.POST.get("username")) #None
+    #     #print(request.POST["username2"]) #Raise error
+    # elif request.method == "GET":
+    #     form = TestForm()
+    #     print(request.GET)
     return render(request, "forms.html", {"form": form})
