1f55538162e7b34cbd52717a36227a5649debe3c
CFE <hello@teamcfe.com>
2016-03-04 13:57:56 -0800

    11 - Initial Form Data

--- src/blog/forms.py
+++ src/blog/forms.py
@@ -5,8 +5,14 @@ from django import forms
 class TestForm(forms.Form):
     some_text = forms.CharField()
     boolean = forms.BooleanField()
-    integer = forms.IntegerField()
+    integer = forms.IntegerField(initial=101)
     email = forms.EmailField(min_length=10)
+
+    def __init__(self, user=None, *args, **kwargs):
+        super(TestForm, self).__init__(*args, **kwargs)
+        print(user)
+        if user:
+            self.fields["some_text"].initial = user.username
 
     def clean_integer(self, *args, **kwargs):
         integer = self.cleaned_data.get("integer")
@@ -16,6 +22,6 @@ class TestForm(forms.Form):
 
     def clean_some_text(self, *args, **kwargs):
         some_text = self.cleaned_data.get("some_text")
-        if len(some_text) < 10:
-            raise forms.ValidationError("Ensure the text is greater than 10 characters")
+        if len(some_text) < 5:
+            raise forms.ValidationError("Ensure the text is greater than 5 characters")
         return some_text
--- src/blog/views.py
+++ src/blog/views.py
@@ -5,19 +5,27 @@ from django.shortcuts import render
 from .forms import TestForm
 
 def home(request):
-    form = TestForm(request.POST or None)
-    if form.is_valid():
-        print(form.cleaned_data)
-        print(form.cleaned_data.get("some_text"))
-        print(form.cleaned_data.get("email"))
-        print(form.cleaned_data.get("email2"))
+    # initial_dict = {
+    #     #"some_text": "Text",
+    #     "boolean": True,
+    #     # "integer": "123"
+    # }
+    # form = TestForm(request.POST or None, initial=initial_dict)
+    # if form.is_valid():
+    #     print(form.cleaned_data)
+    #     print(form.cleaned_data.get("some_text"))
+    #     print(form.cleaned_data.get("email"))
+    #     print(form.cleaned_data.get("email2"))
 
-    # if request.method == "POST":
-    #     form = TestForm(request.POST)
-    #     print(request.POST)
-    #     print(request.POST.get("username")) #None
-    #     #print(request.POST["username2"]) #Raise error
-    # elif request.method == "GET":
-    #     form = TestForm()
-    #     print(request.GET)
+    if request.method == "POST":
+        form = TestForm(data=request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            print(form.cleaned_data.get("some_text"))
+        #print(request.POST)
+        #print(request.POST.get("username")) #None
+        #print(request.POST["username2"]) #Raise error
+    elif request.method == "GET":
+        form = TestForm(user=request.user)
+        print(request.GET)
     return render(request, "forms.html", {"form": form})
