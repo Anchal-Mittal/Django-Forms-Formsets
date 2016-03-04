9dc75a6cdc8bb5eabb93a6bdfbc08f4a5f54cf30
CFE <hello@teamcfe.com>
2016-03-04 15:45:26 -0800

    13 - Model Form

--- src/blog/forms.py
+++ src/blog/forms.py
@@ -1,5 +1,66 @@
 from django import forms
 
+
+from .models import Post
+
+"""
+user = models.ForeignKey(settings.AUTH_USER_MODEL, default=1)
+title = models.CharField(max_length=120)
+slug = models.SlugField(unique=True)
+image = models.FileField(upload_to=upload_location, 
+        null=True, 
+        blank=True, 
+        # width_field="width_field", 
+        # height_field="height_field"
+        )
+        
+height_field = models.IntegerField(default=0)
+width_field = models.IntegerField(default=0)
+content = models.TextField()
+draft = models.BooleanField(default=False)
+publish = models.DateField(auto_now=False, auto_now_add=False)
+updated = models.DateTimeField(auto_now=True, auto_now_add=False)
+timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
+"""
+
+class PostModelForm(forms.ModelForm):
+    #date_field = forms.DateField(initial="2010-11-20", widget=forms.SelectDateWidget(years=YEARS))
+    class Meta:
+        model = Post
+        fields = [
+            "user", 
+            "title", 
+            "slug", 
+            "image"
+            ]
+        #exclude = ["height_field", "width_field"]
+
+    def clean_title(self, *args, **kwargs):
+        title = self.cleaned_data.get("title")
+        print title
+        #raise forms.ValidationError("Nope!")
+        return title
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 SOME_CHOICES = [
         ('db-value', 'Display Value'),
         ('db-value2', 'Display Value2'),
--- src/blog/views.py
+++ src/blog/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from .forms import TestForm
+from .forms import TestForm, PostModelForm
 
 def home(request):
+    form = PostModelForm(request.POST or None)
+    if form.is_valid():
+        form.save()
     # initial_dict = {
     #     #"some_text": "Text",
     #     "boolean": True,
@@ -17,15 +20,16 @@ def home(request):
     #     print(form.cleaned_data.get("email"))
     #     print(form.cleaned_data.get("email2"))
 
-    if request.method == "POST":
-        form = TestForm(data=request.POST)
-        if form.is_valid():
-            print(form.cleaned_data)
-            print(form.cleaned_data.get("some_text"))
-        #print(request.POST)
-        #print(request.POST.get("username")) #None
-        #print(request.POST["username2"]) #Raise error
-    elif request.method == "GET":
-        form = TestForm(user=request.user)
-        print(request.GET)
+    # if request.method == "POST":
+    #     form = TestForm(data=request.POST)
+    #     if form.is_valid():
+    #         print(form.cleaned_data)
+    #         print(form.cleaned_data.get("some_text"))
+    #     #print(request.POST)
+    #     #print(request.POST.get("username")) #None
+    #     #print(request.POST["username2"]) #Raise error
+    # elif request.method == "GET":
+    #     form = TestForm(user=request.user)
+    #     print(request.GET)
+
     return render(request, "forms.html", {"form": form})
