31ab250b72941c54ef959e4210769a272c5deee7
CFE <hello@teamcfe.com>
2016-03-04 14:32:55 -0800

    12 - Form Field Label & Widgets

--- src/blog/forms.py
+++ src/blog/forms.py
@@ -1,11 +1,21 @@
 from django import forms
 
+SOME_CHOICES = [
+        ('db-value', 'Display Value'),
+        ('db-value2', 'Display Value2'),
+        ('db-value3', 'Display Value3'),
+    ]
 
+INTS_CHOICES = [tuple([x,x]) for x in range(0, 102)]
+
+YEARS = [x for x in range(1980, 2031)]
 
 class TestForm(forms.Form):
-    some_text = forms.CharField()
+    date_field = forms.DateField(initial="2010-11-20", widget=forms.SelectDateWidget(years=YEARS))
+    some_text = forms.CharField(label='Text', widget=forms.Textarea(attrs={"rows": 4, "cols": 10}))
+    choices = forms.CharField(label='Text', widget=forms.Select(choices=SOME_CHOICES))
     boolean = forms.BooleanField()
-    integer = forms.IntegerField(initial=101)
+    integer = forms.IntegerField(initial=101, widget=forms.Select(choices=INTS_CHOICES))
     email = forms.EmailField(min_length=10)
 
     def __init__(self, user=None, *args, **kwargs):
